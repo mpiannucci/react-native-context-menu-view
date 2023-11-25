33e541522fc6b51f8a29311019418100ce1c7c84
Matthew Iannucci <mpiannucci@gmail.com>
2023-11-25 11:30:22 -0500

    Add event for tapping preview (#101)

--- ios/ContextMenu.m
+++ ios/ContextMenu.m
@@ -12,6 +12,7 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_VIEW_PROPERTY(title, NSString)
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onPreviewPress, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onCancel, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(actions, NSArray<ContextMenuAction>)
 RCT_EXPORT_VIEW_PROPERTY(disabled, BOOL)
